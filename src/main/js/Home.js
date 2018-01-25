822089272575838fdf9a5564dbb2260965471f8c
napassorn <na.pas.sorn@hotmail.com>
2018-01-26 06:41:48 +0700

    fix #1568 - add CancelRoom.js and update Home,app_old_syntax.js

--- src/main/js/Home.js
+++ src/main/js/Home.js
@@ -3,13 +3,14 @@ var ReactDOM = require('react-dom');
 var ons = require('onsenui');
 var Ons = require('react-onsenui');
 var client = require('./client');
-
+import CancelRoom from './CancelRoom';
 
 let imgUrl = 'https://www.picz.in.th/images/2018/01/25/2000.jpg';
 export default class Home extends React.Component {
+
    renderToolbar() {
     return (
-      <Ons.Toolbar style={{backgroundColor: '#00BFFF'}}>
+      <Ons.Toolbar style={{backgroundColor: '#996699'}}>
 
         <div className='center'> ยินต้อนรับ </div>
       </Ons.Toolbar>
@@ -23,22 +24,25 @@ export default class Home extends React.Component {
   PageBallRoom() {
     this.props.navigator.pushPage({ component: BallRoom, props: { key: 'ballroom' } });
   }
+  PageCancelRoom() {
+    this.props.navigator.pushPage({ component: CancelRoom, props: { key: 'cancelRoom' } });
+  }
 
 render() {
     return (
       <Ons.Page renderToolbar={this.renderToolbar.bind(this)}>
       <Ons.Card style={{paddingLeft:'33%', backgroundImage: 'url(' + imgUrl + ')' }}>
-         <Ons.Card  style={{backgroundColor:'#CCFFE4B5',width: '50%'}}>
+         <Ons.Card  style={{backgroundColor:'#D3D3D3',width: '50%'}}>
 
 
         <div style={{ textAlign: 'center'}}>
           <br />
 
-          <Ons.Button style={{ width: '45%',backgroundColor: 'red' }}>จองห้องจัดเลี้ยง</Ons.Button><br /><br />
-          <Ons.Button style={{ width: '45%',backgroundColor: 'blue' }}>จองห้องพัก</Ons.Button><br /><br />
-          <Ons.Button style={{ width: '45%',backgroundColor: 'green' }}>ยกเลิกห้องพัก</Ons.Button><br /><br />
-          <Ons.Button style={{ width: '45%',backgroundColor: '#CC33CC' }}>แจ้งทำความสะอาด</Ons.Button><br /><br />
-          <Ons.Button style={{ width: '45%',backgroundColor: '#FF3300' }}>แจ้งซ่อม</Ons.Button><br /><br />
+          <Ons.Button style={{ width: '45%',backgroundColor: '#00BFFF' }}>จองห้องจัดเลี้ยง</Ons.Button><br /><br />
+          <Ons.Button style={{ width: '45%',backgroundColor: '#00BFFF' }}>จองห้องพัก</Ons.Button><br /><br />
+          <Ons.Button style={{ width: '45%',backgroundColor: '#00BFFF' }} onClick={this.PageCancelRoom.bind(this)}>แจ้งยกเลิกห้องพัก</Ons.Button><br /><br />
+          <Ons.Button style={{ width: '45%',backgroundColor: '#00BFFF' }}>แจ้งทำความสะอาด</Ons.Button><br /><br />
+          <Ons.Button style={{ width: '45%',backgroundColor: '#00BFFF' }}>แจ้งซ่อม</Ons.Button><br /><br />
           <Ons.Button style={{ width: '45%',backgroundColor: '#00BFFF' }}>ชำระเงิน</Ons.Button>
 
         </div>
