d9497b2e1a3bdf574b9f533d5a9a46f908735afa
Waranya Limsiriwat <jalwaranya321@gmail.com>
2018-01-26 11:34:40 +0700

    fix #1735 - add and update

--- src/main/java/com/example/demo/RoomnumberRepository.java
+++ src/main/java/com/example/demo/RoomnumberRepository.java
@@ -1,7 +1,11 @@
 package com.example.demo;
+import org.springframework.data.repository.query.Param;
 
 import org.springframework.data.repository.CrudRepository;
 
 public interface RoomnumberRepository extends CrudRepository<Roomnumber, Long> {
 
+  Roomnumber findByRoomID(
+          @Param("roomID") String roomID
+  );
 }
